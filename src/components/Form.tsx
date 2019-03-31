e9103335747b6c91cc23a71743e79dc3b6734c32
Zhuyb <919220608@qq.com>
2019-03-31 21:20:28 +0800

    组件类中传递props参数设置state类型

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,13 +4,23 @@ interface IProps {
     name:string;
     age:number;
 }
+interface IState {
+    name:string;
+    age:number;
+}
 
-class Form extends Component <IProps>{
+
+class Form extends Component <IProps,IState>{
+    state = {
+        name:'zhhhh',
+        age:13,
+    }
     render (){
         return (
             <div>
                 detail
                 {this.props.name}+{this.props.age}
+                <p>{this.state.name}+{this.state.age}</p>
             </div>
         )
     }
