7f7086fcaee4eeccbd59a5b252694851b5b85afb
Zhuyb <919220608@qq.com>
2019-04-01 23:19:10 +0800

    棋盘搭建理解属性初始化器语法相当于隐式使用了.bind()方法,使异步函数中的this绑定了组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Form from './components/Form';
-import Func from './components/Func';
+// import Form from './components/Form';
+// import Func from './components/Func';
 
 class App extends Component {
+  state = {
+    board: [0,0,0,0,0,0,0,0,0]
+  }
+
+  public renderCell= (index:number) => {
+    return (
+      <div className="cell">
+      </div>
+    )
+  }
+  public renderBoard=() => {
+    return (
+      <div className="board-container">
+      {this.state.board.map((val,key) => this.renderCell(key))}
+      </div>
+    )
+  }
   render() {
     return (
       <div className="App">
-      <Form name={'string'} age={12}></Form>   
-      <Func name={'zhhhhhhhhhh'}></Func> 
+      {/* <Form name={'string'} age={12}></Form>   
+      <Func name={'zhhhhhhhhhh'}></Func>  */}
+       {this.renderBoard()}
       </div>
     );
   }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -7,6 +7,7 @@ interface IProps {
 interface IState {
     name:string;
     age:number;
+    [key:string]:string | number;
 }
 
 // class Form extends Component <{},IState>//若是没有props则将该参数设为空 {}；                           
@@ -15,12 +16,20 @@ class Form extends Component <IProps,IState>{
         name:'zhhhh',
         age:13,
     }
+    onChange = (e:React.FormEvent<HTMLInputElement>) => {
+        const {name,value } =e.currentTarget;
+        this.setState({
+            [name]:value,
+        })
+    }
     render (){
         return (
             <div>
                 detail
                 {this.props.name}+{this.props.age}
                 <p>{this.state.name}+{this.state.age}</p>
+                <input onChange={this.onChange} type="text" name="name" value={this.state.name}/>
+                <input onChange={this.onChange} type="text" name="age" value={this.state.age}/>
             </div>
         )
     }
